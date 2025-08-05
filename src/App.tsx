8841937dd65e41be143de54e931af9a25fafec56
Ann Kravets <kravetsanna78@gmail.com>
2025-08-05 11:11:26 +0300

    commit master

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import './App.css'
-import MyComponent from "./components/MyComponent.tsx";
 
 function App() {
 
   return (
     <>
 
-        <MyComponent text={'Hello Ann!'} />
-        <MyComponent text={'Hello Okten!'} />
-        <MyComponent text={'Hello World!'} />
-        <MyComponent text={'Have a nice day, Ann!'} />
-        {/*{MyComponent({text: 'Hello Ann'})}*/}
 
     </>
   )
