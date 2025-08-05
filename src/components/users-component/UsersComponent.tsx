39269005820ca43a4c7101789dff5767ac5db78f
Ann Kravets <kravetsanna78@gmail.com>
2025-08-05 13:36:15 +0300

    lesson 2 - service layor

--- src/components/users-component/UsersComponent.tsx
+++ src/components/users-component/UsersComponent.tsx
@@ -1,15 +1,20 @@
 import {useEffect, useState} from "react";
 import type {IUser} from "../../models/IUser.ts";
 import {UserComponent} from "../user-component/UserComponent.tsx";
+import {getUsers} from "../../services/api.service.ts";
 
 export const UsersComponent = () => {
     const [users, setUsers] = useState<IUser[]>([]);
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(value => value.json())
-            .then(response => {
-                setUsers(response);
-            });
+        const fetchData = async () => {
+            const users = await getUsers();
+            setUsers(users);
+        }
+        fetchData();
+        // getUsers()
+        //     .then(response => {
+        //         setUsers(response);
+        //     });
 
         return () => {
             console.log('done');
---
+++ src/services/api.service.ts
@@ -0,0 +1,14 @@
+import type {IUser} from "../models/IUser.ts";
+
+export const getUsers = async (): Promise<IUser[]> => {
+    return await fetch('https://jsonplaceholder.typicode.com/users')
+        .then(value => value.json());
+
+}
+
+ const getUser = async (id: string): Promise<IUser> => {
+    return await fetch('https://jsonplaceholder.typicode.com/users/' + id)
+        .then(value => value.json());
+
+}
+ export { getUser };
