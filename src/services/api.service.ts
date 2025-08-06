d1a07eb1a5c75c867eb178c7d4436aa8497b83ad
Ann Kravets <kravetsanna78@gmail.com>
2025-08-06 11:24:37 +0300

    homework-2 task2-Todos Posts

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,6 +1,15 @@
 import type {IPost} from "../models/IPost.ts";
+import type {ITodo} from "../models/ITodo.ts";
+
+const endpointTodos = import.meta.env.VITE_API_URL + '/todos'
+const endpointPosts = import.meta.env.VITE_API_URL + '/posts'
+
+export const getsTodos = async (): Promise<ITodo[]> => {
+        return await fetch(endpointTodos)
+            .then(value => value.json());
+}
 
 export const loadPosts = async (): Promise<IPost[]> => {
-        return await fetch(import.meta.env.VITE_API_URL)
+        return await fetch(endpointPosts)
             .then(value => value.json());
 }
