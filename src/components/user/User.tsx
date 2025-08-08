5e2b736ffe132d9affdcbedb50a30b2509d7fa4e
Ann Kravets <kravetsanna78@gmail.com>
2025-08-08 18:15:59 +0300

    lesson3 part3 - state-forward

--- src/components/user/User.tsx
+++ src/components/user/User.tsx
@@ -1,15 +1,24 @@
 import type {IUser} from "../../models/UserModel.ts";
 import type {FC} from "react";
+import {Link, useNavigate} from "react-router-dom";
 
 type UserTypeProps = {
     user: IUser
 }
 export const User: FC<UserTypeProps> = ({user}) => {
+
+    const navigate = useNavigate();
+    const handleOnClick = () => {
+        navigate('details', {state: user})
+    }
+
     return (
         <div>
-            {
-                user.username
-            }
+
+                <Link to={'details'} state={user}>{user.name}</Link>
+
+            <button onClick={handleOnClick}>{'Go to details'}</button>
+
         </div>
     );
 };
