27380b92c85b45612b8c888583b11b9ec03e9e1f
Ann Kravets <kravetsanna78@gmail.com>
2025-08-08 12:41:32 +0300

    lesson3 - basic router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(<App/>)
+const router = createBrowserRouter([
+    // {path: '/', element: <MainLayout/>},
+    // {path: '/a', element: <div>layout a</div>},
+    // {path: '/b', element: <div>layout b</div>}
+]);
+
+
+createRoot(document.getElementById('root')!)
+    .render(<RouterProvider router={router}/>)
