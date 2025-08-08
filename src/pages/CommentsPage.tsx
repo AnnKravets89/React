aa64ca19d8e04cd809cd32e805b35231aaa4ab79
Ann Kravets <kravetsanna78@gmail.com>
2025-08-08 23:45:40 +0300

    homework 3 task1

---
+++ src/pages/CommentsPage.tsx
@@ -0,0 +1,11 @@
+import Comments from "../components/comments/Comments.tsx";
+
+const CommentsPage = () => {
+    return (
+        <div>
+            <Comments/>
+        </div>
+    );
+};
+
+export default CommentsPage;
