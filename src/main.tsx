ba2c3d7c6833a6a008e8bef970ddf1daeb4a8aab
Ann Kravets <kravetsanna78@gmail.com>
2025-08-15 10:42:43 +0300

    main commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import {routes} from "./router/routers.tsx";
+import App from "./App.tsx";
+
+
+
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={routes}/>);
+    .render(<App/>);
 
